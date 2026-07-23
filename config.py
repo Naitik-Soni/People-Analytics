834975070f0d0ed339d12f92ac50900aa4661079
Naitik Soni <naitiksoni1705@gmail.com>
2026-07-23 22:15:18 +0530

    Changed logger config and preprocessor

--- config.py
+++ config.py
@@ -5,11 +5,14 @@ Config module for loading every config used in the application
 import json
 from pathlib import Path
 
+from logger import logger
+
 class Config:
     "Config class for getting config file"
     def __init__(self):
         path = Path(__file__).parent.parent / "config.json"
 
+        logger.debug("Config loaded...")
         with open(path, "r", encoding="utf-8") as f:
             self._config = json.load(f)
 
@@ -21,6 +24,7 @@ class Config:
             value = value.get(key)
             if value is None:
                 return default
+        logger.debug(f"Getting config for {key}={value}")
         return value
 
 config = Config()
--- preprocessor.py
+++ preprocessor.py
@@ -1,10 +1,18 @@
 """Video frame preprocessor for preprocessing the frame before analysis pipeline"""
 import cv2
 from cv2.typing import MatLike
+
 from config import config
+from logger import logger
+
+# Function used to resize the frame with specific height and width
+def resize_frame(frame: MatLike , new_size: tuple = (1024, 1024)):
+    logger.debug(f"Resizing the frame with size={new_size}")
+    return cv2.resize(frame, new_size, interpolation=cv2.INTER_LINEAR)
 
 # Function used to resize the frame with specific resize factor
 def resize_frame(frame: MatLike , resize_factor: float = config.get("resize_factor")):
     height, width = frame.shape[:2]
     new_height, new_width = int(height*resize_factor), int(width*resize_factor)
+    logger.debug(f"Resizing the frame with factor={resize_factor}")
     return cv2.resize(frame, (new_width, new_height))
